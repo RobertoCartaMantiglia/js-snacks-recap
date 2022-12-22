9344e5134d7f40914d3001b22e1956db817b9d7a
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-22 10:00:37 +0100

    start

---
+++ script.js
@@ -0,0 +1,4 @@
+/**
+Creare due div; uno conterrà numeri dispari di colore rosso e l'altro conterrà numeri pari in verde.
+ Ad ogni click di un bottone chiedere all'API un numero, se è dispari metterlo nel blocco dispari, e se è pari in quello pari.
+*/
