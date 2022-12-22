d2a53d443de66798f69bd31c9a2958919c386ef5
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-22 17:42:37 +0100

    start snack 4

---
+++ snack4/script4.js
@@ -0,0 +1,3 @@
+/** Chiedere all'utente quanti elementi vuole includere nell'array.
+ Generare attraverso l'API (array/integers) un array contenente N numeri compresi tra 1 e 100.
+ Stampare a schermo la somma dei numeri generati. */
