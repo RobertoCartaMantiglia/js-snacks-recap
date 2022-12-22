bf581cbdef8c2bb3a67c898f52ab94133a89d9f6
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-22 17:53:46 +0100

    add axios i js

--- snack4/script4.js
+++ snack4/script4.js
@@ -1,3 +1,22 @@
 /** Chiedere all'utente quanti elementi vuole includere nell'array.
  Generare attraverso l'API (array/integers) un array contenente N numeri compresi tra 1 e 100.
  Stampare a schermo la somma dei numeri generati. */
+ const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+   
+  }
+}).mount('#app')
+
+
+axios.get('https://flynn.boolean.careers/exercises/api/random/int ')
+    .then( (response) =>{
+        console.log(response.data.response);
+        
+    });
