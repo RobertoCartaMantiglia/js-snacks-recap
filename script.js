5c50fba801e4b5d446cdba6dfd7af4b5d666a03c
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-22 10:13:31 +0100

    add axios

--- script.js
+++ script.js
@@ -2,3 +2,20 @@
 Creare due div; uno conterrà numeri dispari di colore rosso e l'altro conterrà numeri pari in verde.
  Ad ogni click di un bottone chiedere all'API un numero, se è dispari metterlo nel blocco dispari, e se è pari in quello pari.
 */
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      message: 'Hello Vue!',
+      randomNumber: 'null',
+    }
+  },
+  created (){
+    axios.get('https://flynn.boolean.careers/exercises/api/random/int ')
+            .then( (response) =>{
+                this.randomNumber = response.data;
+                console.log(response)
+            });
+  }
+}).mount('#app')
