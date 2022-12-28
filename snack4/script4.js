d227b4a07cd564c215f252548a3ea096ce6e7c91
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-28 16:10:52 +0100

    add newArray methods

--- snack4/script4.js
+++ snack4/script4.js
@@ -1,22 +1,29 @@
 /** Chiedere all'utente quanti elementi vuole includere nell'array.
  Generare attraverso l'API (array/integers) un array contenente N numeri compresi tra 1 e 100.
  Stampare a schermo la somma dei numeri generati. */
- const { createApp } = Vue
+const { createApp } = Vue
 
 createApp({
   data() {
     return {
-      
+      sum: 0,
+      userNumber: '',
     }
   },
   methods: {
-   
-  }
+    newArray(itemNumber) {
+      axios.get('https://flynn.boolean.careers/exercises/api/array/integers?min=1&max=100&items=' + itemNumber)
+        .then((response) => {
+          console.log(response.data.response);
+          let numbers = response.data.response;
+          numbers.forEach(number => {
+            this.sum += number;
+            console.log(this.sum);
+          });
+        });
+    }
+  },
+
 }).mount('#app')
 
 
-axios.get('https://flynn.boolean.careers/exercises/api/random/int ')
-    .then( (response) =>{
-        console.log(response.data.response);
-        
-    });
